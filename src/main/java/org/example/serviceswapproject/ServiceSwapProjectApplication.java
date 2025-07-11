c050cbad7cb6026b4ebcbc83c1ab969d8dfd3f2a
Kana <nphadungsilpe@gmail.com>
2025-07-10 22:28:35 -0700

    Initial commit

---
+++ src/main/java/org/example/serviceswapproject/ServiceSwapProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.serviceswapproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSwapProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSwapProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/serviceswapproject/ServiceSwapProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.serviceswapproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSwapProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
