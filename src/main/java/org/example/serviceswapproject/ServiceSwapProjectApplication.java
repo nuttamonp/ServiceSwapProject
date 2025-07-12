ce37d31ca9d3aec0347bf50e9b4b1792232a095b
Bright Ekeator <bryghtekeator@yahoo.com>
2025-07-11 21:04:24 -0700

    init commit

--- src/main/java/org/example/serviceswapproject/ServiceSwapProjectApplication.java
+++ src/main/java/org/example/serviceswapproject/ServiceSwapProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServiceSwapProjectApplication {
-
+	//I am putting this comment here to push commit for Kana to see if she will receive it.
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceSwapProjectApplication.class, args);
 	}
