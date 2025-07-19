2f74c69764a182ae55d4ea7562aa13be73cff75b
Kana <nphadungsilpe@gmail.com>
2025-07-19 12:06:08 -0700

    Add new branch, html, and add welcome.html

--- src/main/java/org/example/serviceswapproject/ServiceSwapProjectApplication.java
+++ src/main/java/org/example/serviceswapproject/ServiceSwapProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServiceSwapProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceSwapProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSwapProjectApplication.class, args);
+    }
 
 }
